ca5d27ecb4f656b7174b2c6523385559c0028e84
Rafael Camargo <rafaelcamargo.inf@gmail.com>
2021-05-10 23:46:48 -0300

    Create Main.java

---
+++ ATV_2_Injeção_de_Dependência/src/Main.java
@@ -0,0 +1,23 @@
+import java.io.IOException;
+
+public class Main {
+
+	public static void main(String[] args) throws IOException {
+		
+		Aluno aluno = new Aluno();
+		
+		aluno.setNome("Rafa");
+		aluno.setCpf("10875511961");
+		aluno.setMatricula(6);
+		aluno.setEmail("rafaelcamargo.inf@gmail.com");
+		
+		Persistencia_json pjson = new Persistencia_json(aluno);
+		pjson.gerarJson();
+		
+		System.out.println(aluno);
+		System.out.println("\n\n");
+		System.out.println(pjson.saida());
+
+	}
+
+}
