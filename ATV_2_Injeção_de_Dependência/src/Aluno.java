ffe8204c7f267f5653a7edceebf3781b4d5405d6
Rafael Camargo <rafaelcamargo.inf@gmail.com>
2021-05-10 21:27:04 -0300

    update v1.0

--- ATV_2_Injeção_de_Dependência/src/Aluno.java
+++ ATV_2_Injeção_de_Dependência/src/Aluno.java
@@ -1,4 +1,51 @@
 
+
 public class Aluno extends Pessoa {
 
+	//INSTANCIAS
+	
+	
+	//CONSTRUTORES
+	public Aluno() {
+		
+	}
+	
+	public Aluno(String nome) {
+		super(nome);
+	}
+	
+	public Aluno(String nome, int matricula, String cpf, String email) {
+		super(nome);
+		setMatricula(matricula);
+		setCpf(cpf);
+		setEmail(email);
+	}
+	
+	//ATRIBUTOS
+	private int matricula;
+	private String cpf;
+	private String email;
+	
+	//GETTERS E SETTERS
+	public int getMatricula() {
+		return matricula;
+	}
+	public void setMatricula(int matricula) {
+		this.matricula = matricula;
+	}
+	
+	public String getCpf() {
+		return cpf;
+	}
+	public void setCpf(String cpf) {
+		this.cpf = cpf;
+	}
+	
+	public String getEmail() {
+		return email;
+	}
+	public void setEmail(String email) {
+		this.email = email;
+	}
+	
 }
--- ATV_2_Injeção_de_Dependência/src/Pessoa.java
+++ ATV_2_Injeção_de_Dependência/src/Pessoa.java
@@ -1,4 +1,26 @@
 
 public class Pessoa {
 
+	//CONTRUTORES
+	public Pessoa() {
+		
+	}
+	
+	public Pessoa(String nome) {
+		setNome(nome);
+	}
+	
+	//ATRIBUTOS
+	private String nome;
+	
+	//GETTERS E SETTERS
+	public String getNome() {
+		return nome;
+	}
+	public void setNome(String nome) {
+		if(nome.length() < 0) {
+			this.nome = nome;
+		}
+	}
+	
 }
