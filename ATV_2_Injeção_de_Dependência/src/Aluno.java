0271db648f76f5e92930d112bd2d07cf5768feb9
Rafael Camargo <rafaelcamargo.inf@gmail.com>
2021-05-11 00:20:03 -0300

    update v1.4 add data de nascimento

--- ATV_2_Injeção_de_Dependência/src/Aluno.java
+++ ATV_2_Injeção_de_Dependência/src/Aluno.java
@@ -1,5 +1,4 @@
 
-
 public class Aluno extends Pessoa {
 
 	//INSTANCIAS
@@ -14,16 +13,18 @@ public class Aluno extends Pessoa {
 		super(nome);
 	}
 	
-	public Aluno(String nome, int matricula, String cpf, String email) {
+	public Aluno(String nome, int matricula, String cpf, String data_nascimento, String email) {
 		super(nome);
 		setMatricula(matricula);
 		setCpf(cpf);
+		setData_nascimento(data_nascimento);
 		setEmail(email);
 	}
 	
 	//ATRIBUTOS
 	private int matricula;
 	private String cpf;
+	private String data_nascimento;
 	private String email;
 	
 	//GETTERS E SETTERS
@@ -31,21 +32,36 @@ public class Aluno extends Pessoa {
 		return matricula;
 	}
 	public void setMatricula(int matricula) {
-		this.matricula = matricula;
+		if(matricula > 0) {
+			this.matricula = matricula;
+		}	
 	}
 	
 	public String getCpf() {
 		return cpf;
 	}
 	public void setCpf(String cpf) {
-		this.cpf = cpf;
+		if(cpf.length() > 0) {
+			this.cpf = cpf;
+		}	
+	}
+	
+	public String getData_nascimento() {
+		return data_nascimento;
+	}
+	public void setData_nascimento(String data_nascimento) {
+		if(data_nascimento.length() > 0) {
+			this.data_nascimento = data_nascimento;
+		}
 	}
 	
 	public String getEmail() {
 		return email;
 	}
 	public void setEmail(String email) {
-		this.email = email;
+		if(email.length() > 0) {
+			this.email = email;
+		}
 	}
 
 	//SAIDA
@@ -56,6 +72,8 @@ public class Aluno extends Pessoa {
 		builder.append(matricula);
 		builder.append(", cpf=");
 		builder.append(cpf);
+		builder.append(", data_nascimento=");
+		builder.append(data_nascimento);
 		builder.append(", email=");
 		builder.append(email);
 		builder.append(", toString()=");
