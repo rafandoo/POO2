86283d17e4046f1ff180c3649298448653885872
Rafael Camargo <rafaelcamargo.inf@gmail.com>
2021-05-12 10:47:40 -0300

    Update Main.java

--- ATV_2_Injeção_de_Dependência/src/com/classes/Main.java
+++ ATV_2_Injeção_de_Dependência/src/com/classes/Main.java
@@ -12,43 +12,54 @@ public class Main {
 	@SuppressWarnings("rawtypes")
 	public static void main(String[] args) throws IOException {
 
+		// INSTANCIAS
+		
+		// INSERSÂO DE DADOS
 		ListAlunos lista = new ListAlunos();
 		
 		Aluno aluno = new Aluno();
-		aluno.setNome("Rafa");
-		aluno.setCpf("10875511961");
+		aluno.setNome("Rafael");
+		aluno.setCpf("108.755.119-61");
 		aluno.setMatricula(6);
 		aluno.setData_nascimento("05/06/2002");
 		aluno.setEmail("rafaelcamargo.inf@gmail.com");
-		
-		
-		Aluno al2 = new Aluno("Tulio", 5, "10153297999", "01/03/1996", "tuliosouzanascimento@hotmail.com");
-
-		Aluno al3 = new Aluno("Natalia", 16, "111.111.111-11", "10/05/2002", "naty.hang@gmail.com");
 		lista.inserir(aluno);
-		lista.inserir(al2);
-		lista.inserir(al3);
 		
+		aluno = new Aluno();
+		aluno.setNome("Tulio");
+		aluno.setCpf("111.111.111-11");
+		aluno.setMatricula(5);
+		aluno.setData_nascimento("01/03/1996");
+		aluno.setEmail("tuliosouzanascimento@hotmail.com");
+		lista.inserir(aluno);
+
+		aluno = new Aluno();
+		aluno.setNome("Natalia");
+		aluno.setCpf("222.222.222-22");
+		aluno.setMatricula(16);
+		aluno.setData_nascimento("10/05/2002");
+		aluno.setEmail("naty.hang@gmail.com");
+		lista.inserir(aluno);
+
+
+		// GERAR ARQUIVO JSON
 		Persistencia_json pjson = new Persistencia_json();
 		pjson.gerarJson(lista.getLista());
 		
-
+		// GERAR ARQUIVO XML
 		Persistencia_xml.gravarXML(lista.getLista());
 		
-		List<Aluno> list = Persistencia_xml.lerXML();
 		
+		// LEITURA ARQUIVOS PERSISTENCIA
+		List<Aluno> list = Persistencia_xml.lerXML();
 		for(Iterator iter = list.iterator(); iter.hasNext();) {
 			Aluno element = (Aluno) iter.next();
 			System.out.println(element.toString() + "\n---");
 		}
 		
-		System.out.println(aluno);
-		System.out.println(al2);
-		System.out.println("\n\n");
-		//System.out.println(pjson.saida());
-		System.out.println("\n");
+		System.out.println("\nalunos.json");
 		System.out.println(pjson.lerJson());
 
+		
 	}
-
 }
