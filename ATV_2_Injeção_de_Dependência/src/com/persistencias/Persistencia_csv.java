e2dc32b8d0f6df13126c43b148140fb523de2782
Rafael Camargo <rafaelcamargo.inf@gmail.com>
2021-05-12 11:53:23 -0300

    update v1.7.5 correções modelo de geração csv

--- ATV_2_Injeção_de_Dependência/src/com/persistencias/Persistencia_csv.java
+++ ATV_2_Injeção_de_Dependência/src/com/persistencias/Persistencia_csv.java
@@ -2,26 +2,19 @@ package com.persistencias;
 
 import com.classes.Aluno;
 import com.opencsv.CSVWriter;
-import com.opencsv.bean.StatefulBeanToCsv;
-import com.opencsv.bean.StatefulBeanToCsvBuilder;
-import com.opencsv.exceptions.CsvDataTypeMismatchException;
-import com.opencsv.exceptions.CsvRequiredFieldEmptyException;
-
 import java.io.FileWriter;
 import java.io.IOException;
-import java.io.Writer;
-import java.nio.file.Files;
-import java.nio.file.Paths;
 import java.util.ArrayList;
-import java.util.Iterator;
 import java.util.List;
 
 public class Persistencia_csv {
 
-	
+	// ATRIBUTOS
 	private static final String CSV_PATH = "csv/alunos.csv";
 	
-	public static void gCsv(List<Aluno> aluno) throws IOException {
+	// METODOS
+	@SuppressWarnings("resource")
+	public static void gerarCsv(List<Aluno> aluno) throws IOException {
 		try {
 			FileWriter fileWriter = new FileWriter(CSV_PATH);
 			CSVWriter csvWriter = new CSVWriter(fileWriter);
@@ -33,51 +26,18 @@ public class Persistencia_csv {
 			data.add(headers);
 			
 			for (int i = 0; i < aluno.size(); i++) {
-				String[] item = {aluno.get(i).getNome() };
+				String[] item = {aluno.get(i).getNome(), 
+						Integer.toString(aluno.get(i).getMatricula()),
+						aluno.get(i).getCpf(),
+						aluno.get(i).getData_nascimento(),
+						aluno.get(i).getEmail()};
 				data.add(item);
 			}
-			
 			csvWriter.writeAll(data);
-			
-			fileWriter.close();
-			//csvWriter.close();
-			
+			fileWriter.close();	
 		} catch (Exception e) {
 			e.printStackTrace();
 		}
-		
-		
-		
-		
-	}
-	
-	public void gerarCSV(List<Aluno> aluno) throws IOException, CsvDataTypeMismatchException, CsvRequiredFieldEmptyException {
-		
-		Writer writer = Files.newBufferedWriter(Paths.get("csv/alunos.csv"));
-        StatefulBeanToCsv<Aluno> beanToCsv = new StatefulBeanToCsvBuilder(writer).build();
 
-        beanToCsv.write(aluno);
-
-        writer.flush();
-        writer.close();
-
-	}
-	
-	public void gerarCsv(List<Aluno> aluno) throws IOException {
-		FileWriter csvWriter = new FileWriter("csv/alunos.csv");
-		
-		//nome mat cpf dt email
-		csvWriter.append("Nome");
-		csvWriter.append(",");
-		csvWriter.append("Matricula");
-		csvWriter.append(",");
-		csvWriter.append("CPF");
-		csvWriter.append(",");
-		csvWriter.append("Data nascimento");
-		csvWriter.append(",");
-		csvWriter.append("email");
-		csvWriter.append("\n");
-	
-		csvWriter.close();
 	}
 }
