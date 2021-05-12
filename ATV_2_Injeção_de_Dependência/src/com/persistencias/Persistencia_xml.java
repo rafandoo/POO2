51937dea3d03c864cde0f795e8495a365e36c3c9
Rafael Camargo <rafaelcamargo.inf@gmail.com>
2021-05-12 01:18:24 -0300

    update v.1.5.1 criação metodo gera xml

--- ATV_2_Injeção_de_Dependência/src/com/persistencias/Persistencia_xml.java
+++ ATV_2_Injeção_de_Dependência/src/com/persistencias/Persistencia_xml.java
@@ -5,6 +5,7 @@ import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.OutputStreamWriter;
 import java.util.ArrayList;
+import java.util.Date;
 import java.util.Iterator;
 import java.util.List;
 import org.jdom.Document;
@@ -28,6 +29,44 @@ public class Persistencia_xml {
 		titulo.setText("Cadastro de Alunos");
 		
 		Element data = new Element("data");
-		data.setText(DataUtil.)
+		data.setText(DataUtil.DataHoraForStringPadraoH(new Date()));
+		
+		config.addContent(titulo);
+		config.addContent(data);
+		
+		for(int i = 0; i < lista.size(); i++) {
+			Element aluno = new Element("aluno");
+			
+			aluno.setAttribute("matricula", String.valueOf(lista.get(i).getMatricula()));
+			
+			Element nome = new Element("nome");
+			nome.setText(lista.get(i).getNome());
+			
+			Element cpf = new Element("cpf");
+			cpf.setText(lista.get(i).getCpf());
+			
+			Element data_nascimento = new Element("data nascimento");
+			data_nascimento.setText(lista.get(i).getData_nascimento());
+			
+			Element email = new Element("email");
+			email.setText(lista.get(i).getEmail());
+			
+			aluno.addContent(nome);
+			aluno.addContent(cpf);
+			aluno.addContent(data_nascimento);
+			aluno.addContent(email);
+		}
+		
+		XMLOutputter xout = new XMLOutputter();
+		
+		try {
+			System.out.println(NOMEDOARQUIVO + ".xml");
+			BufferedWriter arquivo = new BufferedWriter(new OutputStreamWriter(new FileOutputStream(LOCALHOST + NOMEDOARQUIVO + ".xml"), "UTF-8"));
+			xout.output(documento, arquivo);
+			return true;
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+		return false;
 	}
 }
