9c7f7b7781eb29e4ccc0b071193f67bc09176c4b
Rafael Camargo <rafaelcamargo.inf@gmail.com>
2021-05-12 22:35:56 -0300

    update v1.9 criação metodos controle

--- ATV_2_Injeção_de_Dependência/src/com/persistencias/Controle_persistencia.java
+++ ATV_2_Injeção_de_Dependência/src/com/persistencias/Controle_persistencia.java
@@ -1,5 +1,20 @@
 package com.persistencias;
 
+import java.util.List;
+import com.classes.Aluno;
+
 public class Controle_persistencia {
 
+	// ATRIBUTO
+	protected Persistencia persistencia;
+	
+	//CONSTRUTOR
+	public Controle_persistencia(Persistencia persistencia) {
+		this.persistencia = persistencia;
+	}
+	
+	public boolean gerarPersistencia(List<Aluno> aluno) {
+		return this.persistencia.gerarArquivo(aluno);
+	}
+	
 }
