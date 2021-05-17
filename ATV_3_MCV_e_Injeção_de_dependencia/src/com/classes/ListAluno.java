4bf3b8abf16b290ccae41c7bebb2da8a497d06a0
Rafael Camargo <rafaelcamargo.inf@gmail.com>
2021-05-17 19:50:40 -0300

    update v1.0 criação lista de alunos

--- ATV_3_MCV_e_Injeção_de_dependencia/src/com/classes/ListAluno.java
+++ ATV_3_MCV_e_Injeção_de_dependencia/src/com/classes/ListAluno.java
@@ -1,5 +1,20 @@
 package com.classes;
 
-public class ListAluno {
+import java.util.ArrayList;
+import java.util.List;
 
+public class ListAluno {
+	
+	// INSTANCIA
+	List<Aluno> lista = new ArrayList<Aluno>();
+	
+	// GET
+	public List<Aluno> getLista() {
+		return lista;
+	}
+	
+	// METOD
+	public boolean inserir(Aluno aluno) {
+		return(lista.add(aluno));
+	}
 }
