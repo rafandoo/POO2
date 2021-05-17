40fc7200b98a49f6d1b432054a4b7cfe5b05c33b
Rafael Camargo <rafaelcamargo.inf@gmail.com>
2021-05-17 20:05:09 -0300

    update v1.2 persistencia csv

--- ATV_3_MCV_e_Injeção_de_dependencia/src/com/persistencias/Persistencia_csv.java
+++ ATV_3_MCV_e_Injeção_de_dependencia/src/com/persistencias/Persistencia_csv.java
@@ -1,5 +1,10 @@
 package com.persistencias;
 
-public class Persistencia_csv {
+public class Persistencia_csv implements Persistencia {
+	
+	// ATRIBUTO
+	private static final String LOCALHOST = "csv/alunos.csv";
+	
+	// METODO
 
 }
