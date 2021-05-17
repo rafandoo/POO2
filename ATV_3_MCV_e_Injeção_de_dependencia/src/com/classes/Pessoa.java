e512c4ba7ac3aaff5befcf8e4e8a3d42dbe8d416
Rafael Camargo <rafaelcamargo.inf@gmail.com>
2021-05-17 19:44:11 -0300

    update v1.0 metodos pessoa

--- ATV_3_MCV_e_Injeção_de_dependencia/src/com/classes/Pessoa.java
+++ ATV_3_MCV_e_Injeção_de_dependencia/src/com/classes/Pessoa.java
@@ -1,5 +1,35 @@
 package com.classes;
 
 public class Pessoa {
+	
+	// CONSTRUTORES
+	public Pessoa() {
+		
+	}
+	
+	public Pessoa(String nome) {
+		setNome(nome);
+	}
+	
+	// ATRIBUTO
+	private String nome;
+	
+	// GET E SET
+	public String getNome() {
+		return nome;
+	}
+	public void setNome(String nome) {
+		if(nome.length() > 0) {
+			this.nome = nome;
+		}
+	}
 
+	// SAIDA
+	@Override
+	public String toString() {
+		StringBuilder builder = new StringBuilder();
+		builder.append("Nome: ");
+		builder.append(nome);
+		return builder.toString();
+	}
 }
