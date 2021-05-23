d46e7a13273e5845e1bd3cd817f5a194024797f0
Rafael Camargo <rafaelcamargo.inf@gmail.com>
2021-05-23 18:09:50 -0300

    update v1.5 metodos dao

--- ATV_3_MCV_e_Injeção_de_dependencia/src/com/classes/DAO/AlunoDAO.java
+++ ATV_3_MCV_e_Injeção_de_dependencia/src/com/classes/DAO/AlunoDAO.java
@@ -1,5 +1,37 @@
 package com.classes.DAO;
 
+import java.sql.Connection;
+import java.sql.SQLException;
+import java.sql.Statement;
+
+import com.classes.DTO.Aluno;
+import com.classes.conexao.Conexao;
+
 public class AlunoDAO {
 
+	// ATRIBUTO
+	final String NOMEDATABELA = "aluno";
+	
+	// METODOS
+
+	public boolean inserir(Aluno aluno) {	
+		Connection conn = Conexao.getConexao();
+		String sql = "INSERT INTO `alunos`.`" + NOMEDATABELA + "` "
+				+ "(`matricula`, `cpf`, `dataNascimento`, `email`) "
+				+ "VALUES ('"+ aluno.getMatricula() +"', '"+ aluno.getCpf() +"', '"+ aluno.getData_nascimento() +"' , '"+ aluno.getEmail() +"');";
+		try {
+			Statement stmt = conn.createStatement();
+			stmt.executeUpdate(sql);
+			Conexao.close(conn, stmt);
+			return true;
+		} catch (SQLException e) {
+            System.err.println("Erro: " + e.toString());
+            e.printStackTrace();
+            return false;
+		}
+
+	}
 }
+//INSERT INTO `alunos`.`aluno` (`matricula`, `cpf`, `dataNascimento`, `email`) VALUES ('1', '108', '05063', 'tw');
+
+//UPDATE `alunos`.`aluno` SET `matricula` = '202006201' WHERE (`idAluno` = '1');
