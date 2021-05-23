e85e79f9173f922ff732d0b5ef54707b8864d737
Rafael Camargo <rafaelcamargo.inf@gmail.com>
2021-05-23 17:43:17 -0300

    update v1.4.5 metodos fechar banco

--- ATV_3_MCV_e_Injeção_de_dependencia/src/com/classes/conexao/Conexao.java
+++ ATV_3_MCV_e_Injeção_de_dependencia/src/com/classes/conexao/Conexao.java
@@ -28,4 +28,27 @@ public class Conexao {
 		}
 	}
 	
+	public static void close(Connection conn) {
+		try {
+			if(conn != null) {
+				conn.close();
+			}
+		} catch (SQLException e) {
+			System.err.println("Erro: " + e.toString());
+			e.printStackTrace();
+		}
+	}
+	
+	public static void close(Connection conn, Statement stmt) {
+		close(conn);
+		try {
+			if(stmt != null) {
+				stmt.close();
+			}
+		} catch (SQLException e) {
+			System.err.println("Erro: " + e.toString());
+			e.printStackTrace();
+		}
+	}
+	
 }
