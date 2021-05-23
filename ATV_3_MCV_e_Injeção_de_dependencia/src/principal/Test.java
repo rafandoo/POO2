08f589a1008fc6e3a4f6066446062bb924350757
Rafael Camargo <rafaelcamargo.inf@gmail.com>
2021-05-23 18:46:18 -0300

    Update Test.java

--- ATV_3_MCV_e_Injeção_de_dependencia/src/principal/Test.java
+++ ATV_3_MCV_e_Injeção_de_dependencia/src/principal/Test.java
@@ -6,16 +6,35 @@ import com.classes.DTO.Aluno;
 public class Test {
 
 	public static void main(String []args) {
+
+		atualizar();
+	}
+	
+	public static void inserir() {
 		
 		Aluno aluno = new Aluno();
-		
 		aluno.setNome("Rafael");
 		aluno.setCpf("111.111.111-11");
 		aluno.setMatricula(2020006201);
 		aluno.setData_nascimento("05/06/2002");
 		aluno.setEmail("rafaelcamargo.inf@gmail.com");
-		
-		AlunoDAO adao = new AlunoDAO();
-		adao.inserir(aluno);
+		AlunoDAO.inserir(aluno);
+	}
+	
+	public static void excluir() {
+		Aluno aluno = new Aluno();
+		aluno.setId(1);
+		AlunoDAO.excluir(aluno);
+	}
+	
+	public static void atualizar() {
+		Aluno aluno = new Aluno();
+		aluno.setNome("Rafael");
+		aluno.setCpf("111.111.111-11");
+		aluno.setMatricula(2020006201);
+		aluno.setData_nascimento("05/06/2002");
+		aluno.setEmail("rafaelcamargo.inf@gmail.com");
+		aluno.setId(2);
+		AlunoDAO.alterar(aluno);
 	}
 }
