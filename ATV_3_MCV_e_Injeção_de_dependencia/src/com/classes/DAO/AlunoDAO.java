ebded8fd1badd641f30ce0e0b91f8b98b81e7933
Rafael Camargo <rafaelcamargo.inf@gmail.com>
2021-05-24 20:45:31 -0300

    update v1.5.5 metodos e correções

--- ATV_3_MCV_e_Injeção_de_dependencia/src/com/classes/DAO/AlunoDAO.java
+++ ATV_3_MCV_e_Injeção_de_dependencia/src/com/classes/DAO/AlunoDAO.java
@@ -1,21 +1,25 @@
 package com.classes.DAO;
 
 import java.sql.Connection;
+import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
+import java.util.ArrayList;
+import java.util.List;
 
 import com.classes.DTO.Aluno;
 import com.classes.conexao.Conexao;
 
 public class AlunoDAO {
 
-	// ATRIBUTO
+	// ATRIBUTOS
+	final static String NOMEDOBANCO = "alunos";
 	final static String NOMEDATABELA = "aluno";
 	
 	// METODOS
-	public static boolean inserir(Aluno aluno) {	
+	public static boolean insert(Aluno aluno) {	
 		Connection conn = Conexao.getConexao();
-		String sql = "INSERT INTO `alunos`.`" + NOMEDATABELA + "` "
+		String sql = "INSERT INTO `"+ NOMEDOBANCO +"`.`"+ NOMEDATABELA +"` "
 				+ "(`matricula`, `cpf`, `dataNascimento`, `email`) "
 				+ "VALUES ('"+ aluno.getMatricula() +"', '"+ aluno.getCpf() +"', '"+ aluno.getData_nascimento() +"' , '"+ aluno.getEmail() +"');";
 		try {
@@ -30,13 +34,13 @@ public class AlunoDAO {
 		}
 	}
 	
-	public static boolean excluir(Aluno aluno) {
+	public static boolean delete(Aluno aluno) {
 		if(aluno == null || aluno.getId() == null) {
 			System.out.println("Não foi possivel apagar o registro!");
 			return false;
 		}
 		Connection conn = Conexao.getConexao();
-		String sql = "DELETE FROM `alunos`.`"+ NOMEDATABELA +"` "
+		String sql = "DELETE FROM `"+ NOMEDOBANCO +"`.`"+ NOMEDATABELA +"` "
 				+ "WHERE (`idAluno` = '"+ aluno.getId() +"');";
 		try {
 			Statement stmt = conn.createStatement();
@@ -50,13 +54,13 @@ public class AlunoDAO {
 		}
 	}
 	
-	public static boolean alterar(Aluno aluno) {
+	public static boolean update(Aluno aluno) {
 		if(aluno == null || aluno.getId() == null) {
 			System.out.println("Não foi possivel atualizar o registro!");
 			return false;
 		}
 		Connection conn = Conexao.getConexao();
-		String sql = "UPDATE `alunos`.`" + NOMEDATABELA + "` "
+		String sql = "UPDATE `"+ NOMEDOBANCO +"`.`"+ NOMEDATABELA +"` "
 				+ "SET `matricula` = '"+ aluno.getMatricula() +"', "
 					+ "`cpf` = '"+ aluno.getCpf() +"', "
 					+ "`dataNascimento` = '"+ aluno.getData_nascimento() +"', "
@@ -73,4 +77,72 @@ public class AlunoDAO {
             return false;
 		}
 	}
+	
+	public static Aluno selectMatricula(Aluno aluno) {
+		if(aluno == null || aluno.getMatricula() == 0) {
+			return null;
+		}
+		Connection conn = Conexao.getConexao();
+		String sql = "SELECT * FROM "+ NOMEDOBANCO +"."+ NOMEDATABELA
+				+ "WHERE matricula = " + aluno.getMatricula() + ";";
+		try {
+			Statement stmt = conn.createStatement();
+			ResultSet rs = stmt.executeQuery(sql);
+			if(rs.next()) {
+					Aluno aux = new Aluno();
+					aux.setMatricula(rs.getInt(2));
+					aux.setCpf(rs.getString(3));
+					aux.setData_nascimento(rs.getString(4));
+					aux.setEmail(rs.getString(5));
+					Conexao.close(conn, stmt);
+					rs.close();
+					return aux;
+			} else {
+				Conexao.close(conn, stmt);
+				rs.close();
+				return null;
+			}
+		} catch (SQLException e) {
+            System.err.println("Erro: " + e.toString());
+            e.printStackTrace();
+            return null;
+		}
+	}
+	
+	public List<Aluno> selectAll(){
+		Connection conn = Conexao.getConexao();
+        String sql = "SELECT * FROM " + NOMEDOBANCO +"."+ NOMEDATABELA + ";";
+        try {
+			Statement stmt = conn.createStatement();
+			ResultSet rs = stmt.executeQuery(sql);
+			List<Aluno> list_aluno = buildList(rs);
+			Conexao.close(conn, stmt);
+			rs.close();
+			return list_aluno;
+		} catch (SQLException e) {
+            System.err.println("Erro: " + e.toString());
+            e.printStackTrace();
+            return null;
+		}
+	}
+
+	private List<Aluno> buildList(ResultSet rs) {
+		List<Aluno> list_aluno = new ArrayList<Aluno>();
+		try {
+			while(rs.next()) {
+				Aluno aluno = new Aluno();
+				aluno.setMatricula(rs.getInt(2));
+				aluno.setCpf(rs.getString(3));
+				aluno.setData_nascimento(rs.getString(4));
+				aluno.setEmail(rs.getString(5));
+				list_aluno.add(aluno);
+			}
+			return list_aluno;
+		} catch (SQLException e) {
+            System.err.println("Erro: " + e.toString());
+            e.printStackTrace();
+            return null;
+		}
+	}
+		
 }
