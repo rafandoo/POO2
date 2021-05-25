10a3f7e308fad32fac0653401b474cbba0eae0b9
Rafael Camargo <rafaelcamargo.inf@gmail.com>
2021-05-24 21:02:29 -0300

    Update Test.java

--- ATV_3_MCV_e_Injeção_de_dependencia/src/principal/Test.java
+++ ATV_3_MCV_e_Injeção_de_dependencia/src/principal/Test.java
@@ -7,7 +7,7 @@ public class Test {
 
 	public static void main(String []args) {
 
-		atualizar();
+		inserir();
 	}
 	
 	public static void inserir() {
@@ -18,23 +18,23 @@ public class Test {
 		aluno.setMatricula(2020006201);
 		aluno.setData_nascimento("05/06/2002");
 		aluno.setEmail("rafaelcamargo.inf@gmail.com");
-		AlunoDAO.inserir(aluno);
+		System.out.println(AlunoDAO.exist(aluno));
 	}
 	
 	public static void excluir() {
 		Aluno aluno = new Aluno();
 		aluno.setId(1);
-		AlunoDAO.excluir(aluno);
+		AlunoDAO.delete(aluno);
 	}
 	
 	public static void atualizar() {
 		Aluno aluno = new Aluno();
-		aluno.setNome("Rafael");
+		aluno.setNome("tulio");
 		aluno.setCpf("111.111.111-11");
-		aluno.setMatricula(2020006201);
+		aluno.setMatricula(2020006202);
 		aluno.setData_nascimento("05/06/2002");
 		aluno.setEmail("rafaelcamargo.inf@gmail.com");
 		aluno.setId(2);
-		AlunoDAO.alterar(aluno);
+		AlunoDAO.insert(aluno);
 	}
 }
