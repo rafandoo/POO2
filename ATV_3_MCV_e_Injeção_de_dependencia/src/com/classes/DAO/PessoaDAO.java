312c28049009ccccbfc9c5eb4f38758f08a8bb14
Rafael Camargo <rafaelcamargo.inf@gmail.com>
2021-05-24 21:16:45 -0300

    Update PessoaDAO.java

--- ATV_3_MCV_e_Injeção_de_dependencia/src/com/classes/DAO/PessoaDAO.java
+++ ATV_3_MCV_e_Injeção_de_dependencia/src/com/classes/DAO/PessoaDAO.java
@@ -1,36 +1,5 @@
 package com.classes.DAO;
 
-import java.sql.Connection;
-import java.sql.PreparedStatement;
-import java.sql.SQLException;
-import java.sql.Statement;
-
-import com.classes.DTO.Aluno;
-import com.classes.conexao.Conexao;
-
-import com.classes.conexao.Conexao;
-
 public class PessoaDAO {
 
-	// ATRIBUTO
-	final String NOMEDATABELA = "aluno";
-	
-	// METODOS
-
-	public boolean inserir(Aluno aluno) {		
-		try {
-			Connection conn = Conexao.getConexao();
-			String sql = "INSERT INTO `alunos`.`" + NOMEDATABELA + "` "
-					+ "(`matricula`, `cpf`, `dataNascimento`, `email`) "
-					+ "VALUES (?)";
-			Statement stmt = conn.prepareStatement(sql);
-			
-		} catch (SQLException e) {
-            System.err.println("Erro: " + e.toString());
-            e.printStackTrace();
-            return false;
-		}
-
-	}
 }
-//INSERT INTO `alunos`.`aluno` (`matricula`, `cpf`, `dataNascimento`, `email`) VALUES ('1', '108', '05063', 'tw');
