ea024200c59c8fcb9dabc529f0f6bd7c6f9305aa
Rafael Camargo <rafaelcamargo.inf@gmail.com>
2021-05-24 21:17:11 -0300

    update v1.6 metodos BO

--- ATV_3_MCV_e_Injeção_de_dependencia/src/com/classes/BO/AlunoBO.java
+++ ATV_3_MCV_e_Injeção_de_dependencia/src/com/classes/BO/AlunoBO.java
@@ -1,5 +1,39 @@
 package com.classes.BO;
 
-public class AlunoBO {
+import java.util.List;
 
+import com.classes.DAO.AlunoDAO;
+import com.classes.DTO.Aluno;
+import com.persistencias.Persistencia;
+
+public class AlunoBO implements Persistencia{
+
+	public boolean gerarArquivo(List<Aluno> aluno) {
+		try {
+			for(int i = 0; i < aluno.size(); i++) {
+				if(existe(aluno.get(i)) != true) {
+					AlunoDAO.insert(aluno.get(i));
+				}
+			}
+			return true;
+		} catch (Exception e) {
+            System.err.println("Erro: " + e.toString());
+            e.printStackTrace();
+			return false;
+		}
+	}
+	
+	public boolean alterar(Aluno aluno) {
+		return AlunoDAO.update(aluno);
+	}
+	
+	public boolean excluir(Aluno aluno) {
+		return AlunoDAO.delete(aluno);
+	}
+	
+	public boolean existe(Aluno aluno) {
+		return AlunoDAO.exist(aluno);
+	}
+
+	
 }
