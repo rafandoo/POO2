f6e66b3469e0c9cc95b463b8dc2db59417e0cf10
Rafael Camargo <rafaelcamargo.inf@gmail.com>
2021-06-03 20:45:07 -0300

    update criação metodos de E elemento

--- ATV_6_Generics_Pratica/src/exercicio1/Dados.java
+++
@@ -1,5 +0,0 @@
-package exercicio1;
-
-public class Dados<E> {
-
-}
---
+++ ATV_6_Generics_Pratica/src/exercicio1/Elemento.java
@@ -0,0 +1,18 @@
+package exercicio1;
+
+public class Elemento<E> {
+	
+	// ATRIBUTO
+	private E elemento;
+	
+	// CONSTRUTOR
+	public Elemento(E elemento) {
+		this.elemento = elemento;
+	}
+	
+	// GET
+	public E getElemento() {
+		return elemento;
+	}
+
+}
