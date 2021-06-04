143d4d42228a2e5c1f1a1e22b2756b0c8c00cf53
Rafael Camargo <rafaelcamargo.inf@gmail.com>
2021-06-03 21:12:01 -0300

    Update Main.java

--- ATV_6_Generics_Pratica/src/exercicio1/Main.java
+++ ATV_6_Generics_Pratica/src/exercicio1/Main.java
@@ -4,7 +4,19 @@ public class Main {
 
 	public static void main(String[] args) {
 
-
+		Elemento<String> e1 = new Elemento<>("Ola, mundo!");
+		String str = e1.getElemento();
+		
+		Elemento<Integer> e2 = new Elemento<>(6);
+		int i = e2.getElemento();
+		
+		Elemento<Pessoa> e3 = new Elemento<>(new Pessoa("Rafael", 18));
+		Pessoa p = e3.getElemento();
+		
+		System.out.println(str);
+		System.out.println(i);
+		System.out.println(p);
+		
 	}
 
 }
