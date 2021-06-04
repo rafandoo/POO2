9cb12ed4bb131e591f0afc63e6f201e61ac736ab
Rafael Camargo <rafaelcamargo.inf@gmail.com>
2021-06-03 23:19:48 -0300

    Update Main.java

--- ATV_6_Generics_Pratica/src/exercicio4/Main.java
+++ ATV_6_Generics_Pratica/src/exercicio4/Main.java
@@ -4,6 +4,20 @@ public class Main {
 
 	public static void main(String[] args) {
 
+		Elemento<Integer> e1 = new Elemento<Integer>("rafael", 6);
+		Elemento<String> e2 = new Elemento<String>("rafael", "6");
+		Elemento<String> e3 = new Elemento<String>("6", "rafael");
+				
+		
+		ListElemento lista = new ListElemento();
+		
+		System.out.println(lista.inserir(e1));
+		System.out.println(lista.inserir(e2));
+		System.out.println(lista.inserir(e3));
+		
+		System.out.println("\nBuscando pela chave \"rafael\", valor: " 
+		+ lista.buscaChave("rafael").getValor());
+		
 	}
 
 }
