53386f443345d1f052d1bdb5818ec219f24a3b00
Rafael Camargo <rafaelcamargo.inf@gmail.com>
2021-06-03 21:05:01 -0300

    update atributos e afins

--- ATV_6_Generics_Pratica/src/exercicio1/Pessoa.java
+++ ATV_6_Generics_Pratica/src/exercicio1/Pessoa.java
@@ -2,4 +2,37 @@ package exercicio1;
 
 public class Pessoa {
 
+	// CONSTRUTORES
+	public Pessoa() {
+
+	}
+	
+	public Pessoa(String nome, int idade) {
+		setNome(nome);
+		setIdade(idade);
+	}
+	
+	// ATRIBUTOS
+	private String nome;
+	private int idade;
+	
+	// GETTER E SETTERS
+	public String getNome() {
+		return nome;
+	}
+	public void setNome(String nome) {
+		if(nome.length() > 0) {
+			this.nome = nome;
+		}
+	}
+	
+	public int getIdade() {
+		return idade;
+	}
+	public void setIdade(int idade) {
+		if(idade > 0) {
+			this.idade = idade;
+		}
+	}
+	
 }
