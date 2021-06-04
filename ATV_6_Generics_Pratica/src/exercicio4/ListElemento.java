dc89756aee12d2cea84d8b168b0cea7314059855
Rafael Camargo <rafaelcamargo.inf@gmail.com>
2021-06-03 23:12:53 -0300

    Update ListElemento.java

--- ATV_6_Generics_Pratica/src/exercicio4/ListElemento.java
+++ ATV_6_Generics_Pratica/src/exercicio4/ListElemento.java
@@ -6,47 +6,34 @@ import java.util.List;
 @SuppressWarnings("rawtypes")
 public class ListElemento {
 
-	// INSTANCIA
-	List<Elemento> lista = new ArrayList<Elemento>(); 
+
+	private List<Elemento> lista = new ArrayList<Elemento>();
 	
-	// GET
-	public List<Elemento> getLista() {
-		return lista;
+	public boolean inserir(Elemento elemento) {
+		if (!existeChave(elemento)) {
+			return lista.add(elemento);
+		} else {
+			return false;
+		}
 	}
 	
-	// METODOS
-	public boolean inserir(Elemento elemento) {
-		if(equals(elemento.getChave())) {
-			return lista.add(elemento);
+	public Elemento busca(String chave) {
+		for (Elemento elemento : lista) {
+			if(chave.equals(elemento.getChave())) {
+				return elemento;
+			}
+		}
+		return null;
+	}
+	
+	public boolean existeChave(Elemento e) {
+		for (Elemento elemento : lista) {
+			if(e.getChave().equals(elemento.getChave())) {
+				return true;
+			}
 		}
 		return false;
 	}
-
-	@Override
-	public int hashCode() {
-		final int prime = 31;
-		int result = 1;
-		result = prime * result + ((lista == null) ? 0 : lista.hashCode());
-		return result;
-	}
-
-	@Override
-	public boolean equals(Object obj) {
-		if (this == obj)
-			return true;
-		if (obj == null)
-			return false;
-		if (getClass() != obj.getClass())
-			return false;
-		ListElemento other = (ListElemento) obj;
-		if (lista == null) {
-			if (other.lista != null)
-				return false;
-		} else if (!lista.equals(other.lista))
-			return false;
-		return true;
-	}
-	
 	
 	
 }
