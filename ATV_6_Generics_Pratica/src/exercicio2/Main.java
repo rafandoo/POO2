d30c47d206c40111ade0d19cf73b4b25b4c6e973
Rafael Camargo <rafaelcamargo.inf@gmail.com>
2021-06-03 21:38:16 -0300

    Update Main.java

--- ATV_6_Generics_Pratica/src/exercicio2/Main.java
+++ ATV_6_Generics_Pratica/src/exercicio2/Main.java
@@ -4,7 +4,12 @@ public class Main {
 
 	public static void main(String[] args) {
 
+		Produto<Integer> p1 = new Produto<Integer>(613, "COOLER BOX 3700x", 36.4, "01/12/2019", "INDETERMINADO");
+		
+		Produto<String> p2 = new Produto<String>("LKS0565", "TINTA ACRILICA S/BR LINHA OURO 18L", 526.63, "05/06/2018", "25/12/2020");
 
+		System.out.println(p1);
+		System.out.println(p2);
 	}
 
 }
