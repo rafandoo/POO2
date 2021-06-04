393b9f0b1315bbdc34fd646ed90e8fc549ddf173
Rafael Camargo <rafaelcamargo.inf@gmail.com>
2021-06-03 21:50:15 -0300

    update atributos e afins

--- ATV_6_Generics_Pratica/src/exercicio4/Elemento.java
+++ ATV_6_Generics_Pratica/src/exercicio4/Elemento.java
@@ -2,4 +2,29 @@ package exercicio4;
 
 public class Elemento<K, V> {
 
+	// CONSTRUTORES
+	public Elemento(K chave, V valor) {
+		setChave(chave);
+		setValor(valor);
+	}
+	
+	// ATRIBUTOS
+	private K chave;
+	private V valor;
+	
+	// GETTERS E SETTERS
+	public K getChave() {
+		return chave;
+	}
+	public void setChave(K chave) {
+		this.chave = chave;
+	}
+	
+	public V getValor() {
+		return valor;
+	}
+	public void setValor(V valor) {
+		this.valor = valor;
+	}
+	
 }
