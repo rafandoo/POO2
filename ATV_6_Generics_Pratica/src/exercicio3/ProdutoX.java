bf8c4e521ce050305f8e1177ec9356293befe76d
Rafael Camargo <rafaelcamargo.inf@gmail.com>
2021-08-30 16:47:19 -0300

    Update ProdutoX.java

--- ATV_6_Generics_Pratica/src/exercicio3/ProdutoX.java
+++ ATV_6_Generics_Pratica/src/exercicio3/ProdutoX.java
@@ -1,5 +1,42 @@
 package exercicio3;
 
 public class ProdutoX {
+	
+	// ATRIBUTOS
+	private String descricao;
+	private double valor;
+	private int codigo;
+	
+	// CONSTRUTORES
+	public ProdutoX() {
+		
+	}
+	public ProdutoX(String descricao, double valor, int codigo) {
+		setDescricao(descricao);
+		setValor(valor);
+		setCodigo(codigo);
+	}
+	
+	// GETTERS E SETTERS
+	public String getDescricao() {
+		return descricao;
+	}
+	public void setDescricao(String descricao) {
+		this.descricao = descricao;
+	}
+	
+	public double getValor() {
+		return valor;
+	}
+	public void setValor(double valor) {
+		this.valor = valor;
+	}
+	
+	public int getCodigo() {
+		return codigo;
+	}
+	public void setCodigo(int codigo) {
+		this.codigo = codigo;
+	}
 
 }
