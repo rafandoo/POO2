235e06f04b20cd0fb9cd5573da2aa9f830a3c58d
Rafael Camargo <rafaelcamargo.inf@gmail.com>
2021-08-30 16:47:12 -0300

    Update Pedido.java

--- ATV_6_Generics_Pratica/src/exercicio3/Pedido.java
+++ ATV_6_Generics_Pratica/src/exercicio3/Pedido.java
@@ -1,5 +1,23 @@
 package exercicio3;
 
-public class Pedido {
+import java.util.ArrayList;
 
+import java.util.List;
+
+public class Pedido {
+	
+	// ATRIBUTOS
+	//ProdutoX px;
+	
+	// INSTANCIAS
+	List<? extends ProdutoX> produtos = new ArrayList<>();
+
+	// METODOS
+	public boolean inserir(ProdutoX px) {
+		return false;
+		//return produtos.add((Collection<?>) new ProdutoX());
+		
+	}
+	
+	
 }
